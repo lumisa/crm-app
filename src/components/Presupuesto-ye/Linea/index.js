1787ea503f19015502b6358e08c21845303fac36
Yessica Bao <58489884+pepiyu@users.noreply.github.com>
2022-08-24 10:18:30 +0200

    cambios yessica

--- src/components/Presupuesto-ye/Linea/index.js
+++ src/components/Presupuesto-ye/Linea/index.js
@@ -4,15 +4,25 @@ import { useState } from react
 const Linea = () => {
 
     const [products, setProducts] = useState([
-        {concepto: "Trina Solar", precio: 150, ud: 1, total: 150},
+        {key: "trinaSolar", concepto: "Trina Solar", precio: 150, ud: 1, total: 150},
         {concepto: "Inversor", precio: 200, ud: 1, total: 200},
 
     ])
 
 
-    const onChangeUd = (event) => {
+    const onChangeUd = (key, e) => {
 
-        setProducts(...products, event.target.name: event.target.value)
+        const { name, value } = e.target;
+
+        products.forEach((product) => {
+
+            if (product.key === key)
+            {
+                setProducts({...products, [name]: value})
+
+            }
+        })
+
     }
 
 
@@ -23,7 +33,7 @@ const Linea = () => {
             <div class="row form-group">
                 <div class="col-6"><span id="concepto-${key}">${value.concepto} </span></div>
                     <div class="col-1">
-                        <input type="number" onChange={onChangeUd(event)} step="1" class="form-control" value="${presupuestoUd[modalidad][key].ud}" id="ud-${key}" name="ud-${key}" title="Completar" placeholder="" style="width:100%;"/>
+                        <input type="number" name="ud" onChange={onChangeUd(value.key, )} step="1" class="form-control" value="${presupuestoUd[modalidad][key].ud}" id="ud-${key}" name="ud-${key}" title="Completar" placeholder="" style="width:100%;"/>
                     </div>
                     <div class="col-3">
                         <input type="number" step="0.01" class="form-control" value="${value.importe}" id="importe-${key}" name="importe-${key}" title="Completar" placeholder="" style="width:100%;"/>
